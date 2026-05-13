34e26383fc1d68896da7212dbdf2e64f8b624e81
hassanalbasri84 <albasri2010@gmail.com>
2026-05-13 15:22:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bunyan-v1';
+const CACHE_NAME = 'bunyan-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -14,9 +14,21 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
         })
     );
 });
